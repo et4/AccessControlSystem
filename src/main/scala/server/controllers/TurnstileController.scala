c3c3b953391404243e0e39fae3f0214e7b29a4bf
awethon <gorodgorod63@gmail.com>
2018-05-22 02:33:51 +0300

    reworked /action

--- src/main/scala/server/controllers/TurnstileController.scala
+++ src/main/scala/server/controllers/TurnstileController.scala
@@ -1,34 +1,37 @@
 package server.controllers
 
-import _root_.server.services.CardService
-import akka.http.scaladsl.model.StatusCodes.Success
+import java.sql.Timestamp
+import java.time.Instant
+
+import _root_.server.services.{CardService, LoggerService}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Directive1, Route}
+import akka.http.scaladsl.server.Route
 
-import scala.concurrent.Await
-import scala.util.Try
-
-class TurnstileController(cardManager: CardService) extends Controller {
+class TurnstileController(cardManager: CardService, loggerService: LoggerService) extends Controller {
   val routes: Route = {
     path("action") {
       get {
         parameters('cardId.as[String], 'date.as[String], 'event.as[String]) { (cardId, date, event) =>
-          complete {
-            if (Await.result(cardManager.hasAccess(cardId.toInt), timeout))
-              HttpResponse(StatusCodes.OK)
-            else
-              HttpResponse(StatusCodes.Forbidden)
+          onSuccess(cardManager.hasAccess(cardId.toInt)) { value =>
+            complete {
+              loggerService.log(cardId.toInt, Timestamp.from(Instant.ofEpochMilli(date.toLong)), event, value)
+              if (value)
+                HttpResponse(StatusCodes.OK)
+              else
+                HttpResponse(StatusCodes.Forbidden)
+            }
           }
         }
       }
     }
   } ~ path("hasAccess") {
     get {
-      parameters('cardId.as[String]) { (cardId) =>
-        complete {
-          HttpResponse(StatusCodes.OK,
-            entity = HttpEntity(Await.result(cardManager.hasAccess(cardId.toInt), timeout).toString))
+      parameters('cardId.as[String]) { cardId =>
+        onSuccess(cardManager.hasAccess(cardId.toInt)) { value =>
+          complete {
+            HttpResponse(StatusCodes.OK, entity = value.toString)
+          }
         }
       }
     }
